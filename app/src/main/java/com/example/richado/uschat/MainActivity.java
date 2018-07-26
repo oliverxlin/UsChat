6dddb7cff0ba7871d3b19c9014e6ec1e3c4cf951
oliverxlin <16231324@bjtu.edu.cn>
2018-07-26 18:26:29 +0800

    http

--- app/src/main/java/com/example/richado/uschat/MainActivity.java
+++ app/src/main/java/com/example/richado/uschat/MainActivity.java
@@ -1,19 +1,41 @@
 package com.example.richado.uschat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Handler;
+import android.os.Message;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
+import android.content.Context;
 import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private static String PATH ="123.206.45.190:5000/userinfo/";
     EditText ed_name;
     EditText ed_pwd;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +55,10 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                getUser();
+                String name = ed_name.getText().toString();
+                String pwd = ed_pwd.getText().toString();
+                PostThread postThread = new PostThread(name, pwd);
+                postThread.start();
             }
         });
 
@@ -48,32 +73,67 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
-    }
-    public void getUser()
-    {
-        String sql="select * from user where userid=?";
-        Cursor cursor=database.getWritableDatabase().rawQuery(sql, new String[]{ed_id.getText().toString()});
-        if(cursor.moveToFirst())
-        {
 
-            if(ed_pwd.getText().toString().equals(cursor.getString(cursor.getColumnIndex("userpwd"))))
-            {
-                Toast.makeText(this, "登录成功", 5000).show();
+    }
+
+    //子线程：使用POST方法向服务器发送用户名、密码等数据
+    class PostThread extends Thread {
+
+        String name;
+        String pwd;
+
+        public PostThread(String name, String pwd) {
+            this.name = name;
+            this.pwd = pwd;
+        }
+
+        @Override
+        public void run() {
+            HttpClient httpClient = new DefaultHttpClient();
+            String url = PATH;
+            //第二步：生成使用POST方法的请求对象
+            HttpPost httpPost = new HttpPost(url);
+            //NameValuePair对象代表了一个需要发往服务器的键值对
+
+            JSONObject params = new JSONObject();
+            try {
+                params.put("name",this.name);
+                params.put("pwd",this.pwd);
+//		...
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-            else
-            {
-                Toast.makeText(this, "用户名或者密码错误", 5000).show();
+            StringEntity entity;
+            try {
+                //创建代表请求体的对象（注意，是请求体）
+                entity = new StringEntity(params.toString(),"utf-8");
+                entity.setContentEncoding("UTF-8");
+                entity.setContentType("application/json");
+
+                //将请求体放置在请求对象当中
+                httpPost.setEntity(entity);
+                httpPost.setHeader("Content-type","application/json");
+                httpPost.setHeader("Accept", "application/json");
+                //执行请求对象
+                try {
+                    //第三步：执行请求对象，获取服务器发还的相应对象
+                    HttpResponse response = httpClient.execute(httpPost);
+                    //第四步：检查相应的状态是否正常：检查状态码的值是200表示正常
+                    if (response.getStatusLine().getStatusCode() == 200) {
+                        //第五步：从相应对象当中取出数据，放到entity当中
+                        HttpEntity entity_rep = response.getEntity();
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(entity_rep.getContent()));
+                        String result = reader.readLine();
+                        Log.d("HTTP", "POST:" + result);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-        database.close();
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
 
 }
-
-登录界面代码
--- app/src/main/java/com/example/richado/uschat/UserRegister.java
+++ app/src/main/java/com/example/richado/uschat/UserRegister.java
@@ -1,33 +1,53 @@
 package com.example.richado.uschat;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.Handler;
+import android.os.Message;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.os.Bundle;
+import android.content.Context;
+import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
+
+
 
 public class UserRegister extends Activity {
-
+    private static String PATH ="123.206.45.190:5000/register/";
     //注册用户，密码，确认密码
-    EditText edtext;
-    EditText edpwd;
-    EditText edpwd2;
+    EditText ed_name;
+    EditText ed_pwd;
+    EditText ed_pwd2;
 
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
 
         //注册界面的注册按钮
-        Button btnsub=(Button)findViewById(R.id.reg_new);
+        Button btnsub = (Button) findViewById(R.id.reg_new);
         //注册界面三个变量
-        edtext=(EditText)findViewById(R.id.reg_name);
-        edpwd=(EditText)findViewById(R.id.reg_pwd1);
-        edpwd2=(EditText)findViewById(R.id.reg_pwd2);
+        ed_name = (EditText) findViewById(R.id.reg_name);
+        ed_pwd = (EditText) findViewById(R.id.reg_pwd1);
+        ed_pwd2 = (EditText) findViewById(R.id.reg_pwd2);
 
         //注册按钮监听
         btnsub.setOnClickListener(new View.OnClickListener() {
@@ -35,59 +55,76 @@ public class UserRegister extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                setUser();
+                String name = ed_name.getText().toString();
+                String pwd = ed_pwd.getText().toString();
+                String pwd2 = ed_pwd2.getText().toString();
+                PostThread postThread = new PostThread(name, pwd, pwd2);
+                postThread.start();
             }
         });
-//        Button btncancel=(Button)findViewById(R.id.btncancel);
-//        btncancel.setOnClickListener(new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                // TODO Auto-generated method stub
-//                finish();
-//            }
-//        });
+
 
     }
-    private void setUser()
-    {
-        DBHelper database=new DBHelper(UserRegister.this,"LoginInfo",null,1);
 
+    //子线程：使用POST方法向服务器发送用户名、密码等数据
+    class PostThread extends Thread {
 
-        if(edtext.getText().toString().length()<=0||edpwd.getText().toString().length()<=0||edpwd2.getText().toString().length()<=0)
-        {
-            Toast.makeText(this, "用户名或密码不能为空", 5000).show();
-            return;
+        String name;
+        String pwd;
+        String pwd2;
+
+        public PostThread(String name, String pwd,String pwd2) {
+            this.name = name;
+            this.pwd = pwd;
+            this.pwd2 = pwd2;
         }
-        if(edtext.getText().toString().length()>0)
-        {
-            String sql="select * from user where userid=?";
-            Cursor cursor=database.getWritableDatabase().rawQuery(sql, new String[]{edtext.getText().toString()});
-            if(cursor.moveToFirst())
-            {
-                Toast.makeText(this, "用户名已经存在", 5000).show();
-                return;
+
+        @Override
+        public void run() {
+            HttpClient httpClient = new DefaultHttpClient();
+            String url = PATH;
+            //第二步：生成使用POST方法的请求对象
+            HttpPost httpPost = new HttpPost(url);
+            //NameValuePair对象代表了一个需要发往服务器的键值对
+
+            JSONObject params = new JSONObject();
+            try {
+                params.put("name", this.name);
+                params.put("pwd", this.pwd);
+//		...
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            StringEntity entity;
+            try {
+                //创建代表请求体的对象（注意，是请求体）
+                entity = new StringEntity(params.toString(), "utf-8");
+                entity.setContentEncoding("UTF-8");
+                entity.setContentType("application/json");
+
+                //将请求体放置在请求对象当中
+                httpPost.setEntity(entity);
+                httpPost.setHeader("Content-type", "application/json");
+                httpPost.setHeader("Accept", "application/json");
+                //执行请求对象
+                try {
+                    //第三步：执行请求对象，获取服务器发还的相应对象
+                    HttpResponse response = httpClient.execute(httpPost);
+                    //第四步：检查相应的状态是否正常：检查状态码的值是200表示正常
+                    if (response.getStatusLine().getStatusCode() == 200) {
+                        //第五步：从相应对象当中取出数据，放到entity当中
+                        HttpEntity entity_rep = response.getEntity();
+                        BufferedReader reader = new BufferedReader(
+                                new InputStreamReader(entity_rep.getContent()));
+                        String result = reader.readLine();
+                        Log.d("HTTP", "POST:" + result);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
-        if(!edpwd.getText().toString().equals(edpwd2.getText().toString()))
-        {
-            Toast.makeText(this, "两次输入的密码不同", 5000).show();
-            return;
-        }
-        if(database.AddUser(edtext.getText().toString(), edpwd.getText().toString()))
-        {
-            Toast.makeText(this, "用户注册成功", 5000).show();
-            Intent intent=new Intent();
-            intent.setClass(this, MainActivity.class);
-            startActivity(intent);
-        }
-        else
-        {
-            Toast.makeText(this, "用户注册失败", 5000).show();
-        }
-        database.close();
     }
-
 }
-
-界面注册
